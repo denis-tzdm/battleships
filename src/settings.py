dc1ec3214e51ece558668c79c95051efd022fe5e
Denis Lyshchev <denis.lyshchev@gmail.com>
2022-11-24 12:00:25 +0400

    Refactor, add Cells class, show dead zones

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,7 @@
 GRID_SIZE = 6
 SYMBOLS = {0: ' ', 1: '⨯', 2: '⊗', 3: '◻', 4: '◼'}
-# 0: unhit cell, 1: hit cell, 2: unhit ship block, 3: hit ship block
+# 0: unhit cell, 1: hit cell, 2: ship's dead zone,
+# 3: unhit ship block, 4: hit ship block
 SHIP_TYPES = {3: 1, 2: 2, 1: 4}  # {size: count}
 STOP_COMMAND = 'stop'
 PLAYER_NAMES = {0: 'Player', 1: 'AI'}
